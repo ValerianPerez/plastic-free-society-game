62af78f4b41ac260553be601a3b726c860ddfd39
Reveillark <vlt31@hotmail.fr>
2021-08-30 19:21:33 +0200

    Added skill template
    Added class Skill as a ScriptableObject
    Now a Skill can be created from "Create" Menu

---
+++ plastic-free-society-game/Assets/Scripts/SkillTree/Skill.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skill")]
+public class Skill : ScriptableObject
+{
+    public string Description;
+    public Skill[] Requirements;
+    public int price;
+}
